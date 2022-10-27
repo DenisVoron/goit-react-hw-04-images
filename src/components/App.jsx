c8523fbe9569965473de5f19616d223373c2b0b2
DenisVoron <Denis.voronko@gmail.com>
2022-10-27 22:38:15 +0300

    add app func

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+import { useState, useEffect } from "module";
+//import { Component } from 'react';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,11 +8,118 @@ import { Dna } from  'react-loader-spinner'
 import { SectionApp } from './SectionApp/SectionApp';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
-import { ButtonMore } from './Button/Button';
-import { Modal } from './Modal/Modal';
+//import { ButtonMore } from './Button/Button';
+//import { Modal } from './Modal/Modal';
 import FetchImages from '../services/fetch-api';
 
-export class App extends Component {
+
+export const App = () => {
+  const [searchData, setSearchData] = useState('');
+  const [images, setImages] = useState([]);
+  //const [totalHits, setTotalHits] = useState(0);
+  //const [imagesOnPage, setImagesOnPage] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  //const [showModal, setShowModal] = useState(false);
+  //const [currentLargeImageUrl, setCurrentLargeImageUrl] = useState('');
+
+
+  /*useEffect(() => {
+    FetchImages(searchData, page)
+        .then(({ hits, totalHits }) => {
+
+          if (hits.lenght === 0) {
+            //this.setState({ images: [], imagesOnPage: 0, totalHits: 0 });
+            setImages([]);
+            //setImagesOnPage(0);
+            totalHits(0);
+            return Promise.reject(
+              new Error(`There is no image with name ${searchData}`)
+            );
+          }
+
+          const arrayOfImages = createArrayOfImages(hits);
+
+          setImages(arrayOfImages);
+          totalHits(totalHits);
+          //setImagesOnPage(hits.length);
+
+          /*this.setState({
+            images: arrayOfImages,
+            totalHits,
+            imagesOnPage: hits.length,
+          })
+        })
+      //.finally(() => { });
+  }, [page, searchData]);*/
+
+  useEffect(() => {
+    setLoading(true);
+    FetchImages(searchData, page)
+      .then(({ hits }) => {
+
+        const arrayOfImages = createArrayOfImages(hits);
+
+        setImages([...arrayOfImages]);
+
+        /*this.setState(prevState => {
+          return { images: [...prevState.images, ...arrayOfImages] };
+        });*/
+
+        this.setState({
+          imagesOnPage: this.state.images.length,
+        });
+      })
+      .finally(() => this.turnOffLoader());
+
+  }, [page, searchData]);
+
+  const createArrayOfImages = data => {
+    const arrayOfImages = data.map(element => ({
+      id: element.id,
+      webformatURL: element.webformatURL,
+      largeImageURL: element.largeImageURL,
+    }));
+    return arrayOfImages;
+  };
+
+  const handleFormSubmit = searchData => {
+    console.log(searchData);
+    //this.setState({ searchData, page:1 });
+  }
+
+  return (
+    <SectionApp>
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ImageGallery images={images} openModal={() => { }} />
+      <Dna
+        visible={loading}
+        height="80"
+        width="80"
+        ariaLabel="dna-loading"
+        wrapperStyle={{}}
+        wrapperClass="dna-wrapper"
+      />
+      {/*imagesOnPage >= 12 && imagesOnPage < totalHits && (
+          <ButtonMore
+            onClick={()=>{}}
+          />
+        )*/}
+      {/*showModal && <Modal
+          onCloses={()=>{}}
+          imageUrl={currentLargeImageUrl}
+        />*/}
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        draggable
+      />
+    </SectionApp>
+  );
+};
+
+
+/*export class App extends Component {
   state = {
     searchData: '',
     images: [],
@@ -87,6 +195,7 @@ export class App extends Component {
 
 
   handleFormSubmit = searchData => {
+    console.log(searchData);
     this.setState({ searchData, page:1 });
   }
 
@@ -153,4 +262,4 @@ export class App extends Component {
       </SectionApp>
     );
   }
-}
+}*/
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,12 +1,54 @@
-import { Component } from 'react'
-
+import { useState } from 'react';
+//import { Component } from 'react'
 import { toast } from 'react-toastify';
-
 import { ImSearch } from 'react-icons/im';
 
 import css from './Searchbar.module.css';
 
-export class Searchbar extends Component {
+
+export function Searchbar({onSubmit}) {
+    const [searchData, setSearchData] = useState('');
+
+    const handleDataChange = event => {
+        setSearchData(event.currentTarget.value.toLowerCase());
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (searchData.trim() === '') {
+            toast.warn('Введите данные для загрузки')
+            return;
+        }
+
+        onSubmit(searchData);
+        setSearchData('');
+    }
+
+    return (
+          <header className={css.Searchbar} onSubmit={handleSubmit}>
+              <form className={css.SearchForm}>
+                  <button type="submit" className={css.SearchFormButton}>
+                      <ImSearch />
+                  </button>
+
+                  <input
+                      className={css.SearchFormInput}
+                      type="text"
+                      name="searchData"
+                      value={searchData}
+                      onChange={handleDataChange}
+                      autoComplete="off"
+                      autoFocus
+                      placeholder="Search images and photos"
+                  />
+              </form>
+          </header>
+      );
+}
+
+
+
+/*export class Searchbar extends Component {
     state = {
         searchData: ''
     }
@@ -48,4 +90,4 @@ export class Searchbar extends Component {
           </header>
       );
     }
-};
+};*/
